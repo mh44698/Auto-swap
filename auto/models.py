04364306e9770d1cb3e6c505690d9fd5ff1212ce
Mark Holmes <holmesmarkr@johndeere.com>
2020-02-11 11:16:29 -0600

    fixing bugs

--- auto/models.py
+++ auto/models.py
@@ -1,23 +1,23 @@
 from django.db import models
 
-# class Seller(models.Model):
-#     name = models.CharField(default = '', max_length = 200)
-#     phone = models.CharField(default = '', max_length = 20)
-#     address = models.CharField(default = '', max_length = 400)
-#     web_site = models.CharField(default = '', max_length = 100)
-#     
-#     def __str__(self):
-#         return self.name
+class Seller(models.Model):
+    name = models.CharField(default = '', max_length = 200)
+    phone = models.CharField(default = '', max_length = 20)
+    address = models.CharField(default = '', max_length = 400)
+    web_site = models.CharField(default = '', max_length = 100)
+    
+    def __str__(self):
+        return self.name
 
 class Car(models.Model):
-    make = models.CharField(default = '', max_length= 100)
+    make = models.CharField(default = '', max_length= 100)
     model = models.CharField(default = '', max_length= 100)
     trim = models.CharField(default = '', max_length= 100)
     img_url = models.CharField(default = '', max_length= 200)
     milage = models.CharField(default = '', max_length= 100)
     color = models.CharField(default = '', max_length= 100)
     name = models.ForeignKey(Seller, on_delete = models.CASCADE, related_name = 'car')
-
-   def __str__(self):
-        return self.model
+    
+    def __str__(self):
+     return self.model
  
